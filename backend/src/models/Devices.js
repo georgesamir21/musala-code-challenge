dbdae3d2b6f2a22779a68429c14fd6a64185c6a4
George Samir <georges.samir21@gmail.com>
2022-03-19 16:26:21 +0200

    add models

---
+++ backend/src/models/Devices.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+module.exports = () => {
+  const deviceShema = new Schema({
+    uid: {
+      type: Number,
+      required: true,
+      // unique: true,
+      default: Date.now
+    },
+    vendor: {
+      type: String
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: [
+        'online',
+        'offline'
+      ]
+    },
+    gateway: {
+      type: Schema.Types.ObjectId,
+      ref: 'Gateway'
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
+
+  const Device = model('Device', deviceShema);
+  return Device;
+};
---
+++ backend/src/models/Gateways.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+module.exports = () => {
+  const gatewaySchema = new Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    serial_number: {
+      type: String,
+      default: uuidv4,
+      unique: true,
+      required: true
+    },
+    ip_address: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (ip) =>
+          /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+            ip
+          ),
+        message: (data) => `${data.value} is invalid IPV4 address!'`
+      }
+    },
+    devices: [{ type: Schema.Types.ObjectId, ref: 'Device' }]
+  });
+
+  const Gateway = model('Gateway', gatewaySchema);
+  return Gateway;
+};
