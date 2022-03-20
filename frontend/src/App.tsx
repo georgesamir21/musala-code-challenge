1e5da4a5d38ad171ca562e572c9c8b3cf3692706
George Samir <georges.samir21@gmail.com>
2022-03-20 07:30:34 +0200

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Gateway } from "./components/Gateway";
+import { GatewayDetails } from "./components/GatewayDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Gateway />
+      <GatewayDetails />
+    </div>
+  );
+}
+
+export default App;
